96a004d0778827e03f41dba73d28fa384410fed9
EricsonCR <ericson4634@gmail.com>
2025-04-05 01:53:09 -0500

    {v2}, commit 2

--- src/app/pages/home2/home2.component.ts
+++ src/app/pages/home2/home2.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
 import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home2.component.html',
   styleUrl: './home2.component.css'
 })
-export class Home2Component implements OnInit, OnDestroy {
+export class Home2Component implements OnInit {
 
   private menuSubscription!: Subscription;
 
@@ -18,9 +20,11 @@ export class Home2Component implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-    this.menuSubscription = this.sharedService.menu.subscribe(value => {
-      this.scrollToSeccton(value);
-    });
+    if (typeof window !== 'undefined') {
+      this.menuSubscription = this.sharedService.menu.subscribe(value => {
+        this.scrollToSeccton(value);
+      });
+    }
   }
 
   scrollToSeccton(id: string) {
@@ -28,11 +32,6 @@ export class Home2Component implements OnInit, OnDestroy {
     if (section) {
       section.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-  }
-
-  ngOnDestroy(): void {
-    this.menuSubscription.unsubscribe();
-    console.log("obs destroy");
   }
 
 }
